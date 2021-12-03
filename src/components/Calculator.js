e3ec9b88866716591a3d0ab0a51a52b225738b63
aakbarkhan <aakbarkhan@gmail.com>
2021-12-03 11:43:06 +0530

    Change the Btn class component into functional component

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -10,8 +10,9 @@ const Calculator = () => {
   const [operation, setOp] = useState(null);
 
   const handleClick = (buttonName) => {
-    const name = buttonName.target.innerText;
-    const results = calculate({ total, next, operation }, name);
+    // const name = buttonName.target.innerText;
+    const results = calculate({ total, next, operation }, buttonName);
+    // console.log(results);
     setTotal(results.total);
     setNext(results.next);
     setOp(results.operation);
@@ -33,33 +34,33 @@ const Calculator = () => {
     <div className="wrapper">
       <input className="input-field" placeholder="0" type="text" maxLength="0" value={display()} onChange={() => ''} />
       <div className="btn-wrapper">
-        <Btn value="AC" className="btn" click={handleClick} />
-        <Btn value="+" className="btn" click={handleClick} />
-        <Btn value="%" className="btn" click={handleClick} />
-        <Btn value="÷" className="btn light" click={handleClick} />
+        <Btn value="AC" className="btn" onEvent={handleClick} />
+        <Btn value="+/-" className="btn" onEvent={handleClick} />
+        <Btn value="%" className="btn" onEvent={handleClick} />
+        <Btn value="÷" className="btn light" onEvent={handleClick} />
       </div>
       <div className="btn-wrapper">
-        <Btn value="7" className="btn" click={handleClick} />
-        <Btn value="8" className="btn" click={handleClick} />
-        <Btn value="9" className="btn" click={handleClick} />
-        <Btn value="x" className="btn light" click={handleClick} />
+        <Btn value="7" className="btn" onEvent={handleClick} />
+        <Btn value="8" className="btn" onEvent={handleClick} />
+        <Btn value="9" className="btn" onEvent={handleClick} />
+        <Btn value="x" className="btn light" onEvent={handleClick} />
       </div>
       <div className="btn-wrapper">
-        <Btn value="4" className="btn" click={handleClick} />
-        <Btn value="5" className="btn" click={handleClick} />
-        <Btn value="6" className="btn" click={handleClick} />
-        <Btn value="-" className="btn light" click={handleClick} />
+        <Btn value="4" className="btn" onEvent={handleClick} />
+        <Btn value="5" className="btn" onEvent={handleClick} />
+        <Btn value="6" className="btn" onEvent={handleClick} />
+        <Btn value="-" className="btn light" onEvent={handleClick} />
       </div>
       <div className="btn-wrapper">
-        <Btn value="1" className="btn" click={handleClick} />
-        <Btn value="2" className="btn" click={handleClick} />
-        <Btn value="3" className="btn" click={handleClick} />
-        <Btn value="+" className="btn light" click={handleClick} />
+        <Btn value="1" className="btn" onEvent={handleClick} />
+        <Btn value="2" className="btn" onEvent={handleClick} />
+        <Btn value="3" className="btn" onEvent={handleClick} />
+        <Btn value="+" className="btn light" onEvent={handleClick} />
       </div>
       <div className="btn-wrapper">
-        <Btn value="0" className="btn last" click={handleClick} />
-        <Btn value="." className="btn dot" click={handleClick} />
-        <Btn value="=" className="btn light-end" click={handleClick} />
+        <Btn value="0" className="btn last" onEvent={handleClick} />
+        <Btn value="." className="btn dot" onEvent={handleClick} />
+        <Btn value="=" className="btn light-end" onEvent={handleClick} />
       </div>
     </div>
   );
