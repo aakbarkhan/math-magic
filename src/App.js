791bcaae3ca467dc5cbb2acb8f8a1f8a20caf80b
aakbarkhan <aakbarkhan@gmail.com>
2021-12-08 18:17:58 +0530

    Add the home and about page using route

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
 import React from 'react';
+import {
+  BrowserRouter, Routes, Route, Link,
+} from 'react-router-dom';
 import './App.css';
 import Calculator from './components/Calculator';
+import Home from './components/Home';
+import Quote from './components/Quote';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+const App = () => (
+  <BrowserRouter>
+    <div className="wrapper-er">
+      <nav className="nav">
+        <h1 className="heading">Math Magicians</h1>
+        <ul>
+          <Link to="/">Home</Link>
+          <Link to="/calculator">Calculator</Link>
+          <Link to="/quotes">Quote</Link>
 
-  render() {
-    return <Calculator />;
-  }
-}
+        </ul>
+      </nav>
+    </div>
+    <Routes>
+      <Route exact path="/" element={<Home />} />
+      <Route path="/calculator" element={<Calculator />} />
+      <Route path="/quotes" element={<Quote />} />
+
+    </Routes>
+  </BrowserRouter>
+);
 
 export default App;
---
+++ src/components/Quote.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Quote() {
+  return (<h1> Early to bed and early to rise makes a man zombie</h1>);
+}
+
+export default Quote;
